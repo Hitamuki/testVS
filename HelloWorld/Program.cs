4f47e9233790793a357287a425c26fc29405495a
kumataka <hitamuki024@gmail.com>
2022-03-24 15:52:25 +0900

    PJ2からプッシュ

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -9,6 +9,7 @@ namespace HelloWorld
             Console.WriteLine("Hello World!");
             Console.WriteLine("What is your name?");
             String str = "kuma";
+            Console.Write(str);
             var name = Console.ReadLine();
             var currentDate = DateTime.Now;
             Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}!");
